24b9a4cad24af3439896198c9784fa8483727414
ShunchaoChenNBC <117398619+ShunchaoChenNBC@users.noreply.github.com>
2022-11-30 11:40:27 -0500

    Update Watch_Time_Query.sql
    Update the query to include ID level

--- Watch_Time_Query.sql
+++ Watch_Time_Query.sql
@@ -1,38 +1,60 @@
 
+
 with cte as (select
+Adobe_Tracking_ID,
 adobe_timestamp,
-lower(b.Vdo_Titles) as Vdo_Titles,
+lower(b.Display_Name) as Vdo_Titles,
 case when Video_Start_Type = 'Auto-Play' and Next_Actions = 'Vdo_End' then Next_Timestamp - adobe_timestamp end as Auto_Binge_Watch_Time,
 case when Video_Start_Type = 'Up-Next' and Next_Actions = 'Vdo_End' then Next_Timestamp - adobe_timestamp end as Up_Next_Watch_Time,
+case when Video_Start_Type = 'Auto-Play' and Next_Actions = 'Vdo_End' then num_seconds_played_no_ads/3600 end as Auto_Binge_No_Ad_Hours,
+case when Video_Start_Type = 'Up-Next' and Next_Actions = 'Vdo_End' then num_seconds_played_no_ads/3600 end as Up_Next_No_Ad_Hours,
+num_seconds_played_no_ads
 from
 (SELECT a.*,
-lead(Video_Start_Type) over (partition by lower(Vdo_Titles) order by adobe_timestamp) as Next_Actions,
-lead(adobe_timestamp) over (partition by lower(Vdo_Titles) order by adobe_timestamp) as Next_Timestamp,
-FROM `nbcu-ds-sandbox-a-001.Shunchao_Sandbox.Auto-Binge-Sample` a
-where Video_Start_Type is not null
-order by adobe_timestamp) b),
+lead(Video_Start_Type) over (partition by Adobe_Tracking_ID order by adobe_timestamp) as Next_Actions,
+lead(adobe_timestamp) over (partition by Adobe_Tracking_ID order by adobe_timestamp) as Next_Timestamp,
+FROM `nbcu-ds-sandbox-a-001.Shunchao_Sandbox.Auto_Binge_Table`  a
+where Adobe_Tracking_ID is not null
+order by Adobe_Tracking_ID, adobe_timestamp) b),
+
 cte2 as (select 
+Adobe_Tracking_ID,
 adobe_timestamp,
 Vdo_Titles,
-extract(day from Auto_Binge_Watch_Time)*24*60*60+extract(hour from Auto_Binge_Watch_Time)*60*60+extract(minute from Auto_Binge_Watch_Time)*60+extract(second from Auto_Binge_Watch_Time) as Auto_Binge_Watch_Time,
-extract(day from Up_Next_Watch_Time)*24*60*60+extract(hour from Up_Next_Watch_Time)*60*60+extract(minute from Up_Next_Watch_Time)*60+extract(second from Up_Next_Watch_Time) as Up_Next_Watch_Time,
+extract(day from Auto_Binge_Watch_Time)*24+extract(hour from Auto_Binge_Watch_Time)+extract(minute from Auto_Binge_Watch_Time)/60+extract(second from Auto_Binge_Watch_Time)/3600 as Auto_Binge_Watch_Time_Hours,
+extract(day from Up_Next_Watch_Time)*24+extract(hour from Up_Next_Watch_Time)+extract(minute from Up_Next_Watch_Time)/60+extract(second from Up_Next_Watch_Time)/3600 as Up_Next_Watch_Time_Hours,
+Auto_Binge_No_Ad_Hours,
+Up_Next_No_Ad_Hours,
+num_seconds_played_no_ads
 from cte)
 
 
 select
-Auto_Binge_Watch_Time,
-Up_Next_Watch_Time,
-Total_Watch_Time,
-round(Up_Next_Watch_Time/Total_Watch_Time,2) as Prc_Up_Next,
-round(Auto_Binge_Watch_Time/Total_Watch_Time,2) as Prc_Auto_Binge
+CURRENT_DATE(), --Auto_Binge_table should be updated to current date before running this query
+Vdo_Titles,
+sum(Auto_Binge_Watch_Time_Hours) as Total_Auto_Binge_w_Ad,
+sum(Up_Next_Watch_Time_Hours) as Total_Up_Next_w_Ad,
+sum(Total_Watch_Time_w_Ad) as Total_Watch_Time_w_Ad,
+round(sum(Up_Next_Watch_Time_Hours)/sum(Total_Watch_Time_w_Ad),2) as Prc_Up_Next_w_Ad,
+round(sum(Auto_Binge_Watch_Time_Hours)/sum(Total_Watch_Time_w_Ad),2) as Prc_Auto_Binge_w_Ad,
+sum(Auto_Binge_No_Ad_Hours) as Total_Auto_Binge_No_Ad,
+sum(Up_Next_No_Ad_Hours) as Total_Up_Next_No_Ad,
+sum(num_seconds_played_no_ads) as Total_Watch_hour_no_Ad,
+round(sum(Up_Next_No_Ad_Hours)/sum(num_seconds_played_no_ads),2) as Prc_Up_Next_No_Ad,
+round(sum(Auto_Binge_No_Ad_Hours)/sum(num_seconds_played_no_ads),2) as Prc_Auto_Binge_No_Ad
 from (select 
-sum(Auto_Binge_Watch_Time) as Auto_Binge_Watch_Time,
-sum(Up_Next_Watch_Time) as Up_Next_Watch_Time,
-extract(day from (max(adobe_timestamp)-min(adobe_timestamp)))*24*60*60\
-+extract(hour from (max(adobe_timestamp)-min(adobe_timestamp)))*60*60\
-+extract(minute from (max(adobe_timestamp)-min(adobe_timestamp)))*60\
-+extract(second from (max(adobe_timestamp)-min(adobe_timestamp))) as Total_Watch_Time
-from cte2) c
+Vdo_Titles,
+Adobe_Tracking_ID,
+sum(Auto_Binge_Watch_Time_Hours) as Auto_Binge_Watch_Time_Hours,
+sum(Up_Next_Watch_Time_Hours) as Up_Next_Watch_Time_Hours,
+sum(Auto_Binge_No_Ad_Hours) as Auto_Binge_No_Ad_Hours,
+sum(Up_Next_No_Ad_Hours) as Up_Next_No_Ad_Hours,
+extract(day from (max(adobe_timestamp)-min(adobe_timestamp)))*24+extract(hour from (max(adobe_timestamp)-min(adobe_timestamp)))+extract(minute from (max(adobe_timestamp)-min(adobe_timestamp)))/60+extract(second from (max(adobe_timestamp)-min(adobe_timestamp)))/3600 as Total_Watch_Time_w_Ad,
+sum(num_seconds_played_no_ads) as num_seconds_played_no_ads
+from cte2
+group by 1, 2) c
+group by 1,2
+
 
 
 
