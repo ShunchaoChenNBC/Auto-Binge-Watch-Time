e011a552c889163c3a3e6bd0b38ac7e41249d7f7
ShunchaoChenNBC <117398619+ShunchaoChenNBC@users.noreply.github.com>
2022-12-04 21:26:33 -0500

    Update Watch_Time_Query.sql
    Simplify the code

--- Watch_Time_Query.sql
+++ Watch_Time_Query.sql
@@ -1,60 +1,41 @@
-
-
 with cte as (select
+Adobe_Date,
 Adobe_Tracking_ID,
 adobe_timestamp,
 lower(b.Display_Name) as Vdo_Titles,
-case when Video_Start_Type = 'Auto-Play' and Next_Actions = 'Vdo_End' then Next_Timestamp - adobe_timestamp end as Auto_Binge_Watch_Time,
-case when Video_Start_Type = 'Up-Next' and Next_Actions = 'Vdo_End' then Next_Timestamp - adobe_timestamp end as Up_Next_Watch_Time,
-case when Video_Start_Type = 'Auto-Play' and Next_Actions = 'Vdo_End' then num_seconds_played_no_ads/3600 end as Auto_Binge_No_Ad_Hours,
-case when Video_Start_Type = 'Up-Next' and Next_Actions = 'Vdo_End' then num_seconds_played_no_ads/3600 end as Up_Next_No_Ad_Hours,
-num_seconds_played_no_ads
+case when Last_Actions = 'Auto-Play' and Video_Start_Type = 'Vdo_End' then num_seconds_played_no_ads/3600 end as Auto_Binge_Hours,
+case when Last_Actions = 'Clicked-Up-Next' and Video_Start_Type = 'Vdo_End' then num_seconds_played_no_ads/3600 end as Up_Next_Hours,
+num_seconds_played_no_ads/3600 as Watch_Hours
 from
 (SELECT a.*,
-lead(Video_Start_Type) over (partition by Adobe_Tracking_ID order by adobe_timestamp) as Next_Actions,
-lead(adobe_timestamp) over (partition by Adobe_Tracking_ID order by adobe_timestamp) as Next_Timestamp,
-FROM `nbcu-ds-sandbox-a-001.Shunchao_Sandbox.Auto_Binge_Table`  a
-where Adobe_Tracking_ID is not null
-order by Adobe_Tracking_ID, adobe_timestamp) b),
+lag(Video_Start_Type) over (partition by Adobe_Tracking_ID order by adobe_timestamp) as Last_Actions
+FROM `nbcu-ds-sandbox-a-001.Shunchao_Sandbox.Auto_Binge_1126`  a
+where Adobe_Tracking_ID is not null and Video_Start_Type is not null
+order by Adobe_Tracking_ID, adobe_timestamp) b
+where b.Display_Name is not null)
 
-cte2 as (select 
-Adobe_Tracking_ID,
-adobe_timestamp,
+select *
+from 
+(select 
+Adobe_Date,
+'Grand_Total' Vdo_Titles,
+round(sum(Auto_Binge_Hours),4) as Grand_Auto_Binge_Hours,
+round(sum(Up_Next_Hours),4) as Grand_Up_Next_Hours,
+round(sum (Watch_Hours),4) as Grand_Total_Watch_Hours,
+round(SAFE_DIVIDE(sum(Auto_Binge_Hours),sum (Watch_Hours)),4) as Prc_Auto_Binge,
+round(safe_divide(sum(Up_Next_Hours), sum (Watch_Hours)),4) as Prc_Up_Next
+from cte
+group by 1, 2
+union all
+select 
+Adobe_Date,
 Vdo_Titles,
-extract(day from Auto_Binge_Watch_Time)*24+extract(hour from Auto_Binge_Watch_Time)+extract(minute from Auto_Binge_Watch_Time)/60+extract(second from Auto_Binge_Watch_Time)/3600 as Auto_Binge_Watch_Time_Hours,
-extract(day from Up_Next_Watch_Time)*24+extract(hour from Up_Next_Watch_Time)+extract(minute from Up_Next_Watch_Time)/60+extract(second from Up_Next_Watch_Time)/3600 as Up_Next_Watch_Time_Hours,
-Auto_Binge_No_Ad_Hours,
-Up_Next_No_Ad_Hours,
-num_seconds_played_no_ads
-from cte)
-
-
-select
-CURRENT_DATE(), --Auto_Binge_table should be updated to current date before running this query
-Vdo_Titles,
-sum(Auto_Binge_Watch_Time_Hours) as Total_Auto_Binge_w_Ad,
-sum(Up_Next_Watch_Time_Hours) as Total_Up_Next_w_Ad,
-sum(Total_Watch_Time_w_Ad) as Total_Watch_Time_w_Ad,
-round(sum(Up_Next_Watch_Time_Hours)/sum(Total_Watch_Time_w_Ad),2) as Prc_Up_Next_w_Ad,
-round(sum(Auto_Binge_Watch_Time_Hours)/sum(Total_Watch_Time_w_Ad),2) as Prc_Auto_Binge_w_Ad,
-sum(Auto_Binge_No_Ad_Hours) as Total_Auto_Binge_No_Ad,
-sum(Up_Next_No_Ad_Hours) as Total_Up_Next_No_Ad,
-sum(num_seconds_played_no_ads) as Total_Watch_hour_no_Ad,
-round(sum(Up_Next_No_Ad_Hours)/sum(num_seconds_played_no_ads),2) as Prc_Up_Next_No_Ad,
-round(sum(Auto_Binge_No_Ad_Hours)/sum(num_seconds_played_no_ads),2) as Prc_Auto_Binge_No_Ad
-from (select 
-Vdo_Titles,
-Adobe_Tracking_ID,
-sum(Auto_Binge_Watch_Time_Hours) as Auto_Binge_Watch_Time_Hours,
-sum(Up_Next_Watch_Time_Hours) as Up_Next_Watch_Time_Hours,
-sum(Auto_Binge_No_Ad_Hours) as Auto_Binge_No_Ad_Hours,
-sum(Up_Next_No_Ad_Hours) as Up_Next_No_Ad_Hours,
-extract(day from (max(adobe_timestamp)-min(adobe_timestamp)))*24+extract(hour from (max(adobe_timestamp)-min(adobe_timestamp)))+extract(minute from (max(adobe_timestamp)-min(adobe_timestamp)))/60+extract(second from (max(adobe_timestamp)-min(adobe_timestamp)))/3600 as Total_Watch_Time_w_Ad,
-sum(num_seconds_played_no_ads) as num_seconds_played_no_ads
-from cte2
-group by 1, 2) c
-group by 1,2
-
-
-
-
+round(sum(Auto_Binge_Hours),4) as Grand_Auto_Binge_Hours,
+round(sum(Up_Next_Hours),4) as Grand_Up_Next_Hours,
+round(sum (Watch_Hours),4) as Grand_Total_Watch_Hours,
+round(SAFE_DIVIDE(sum(Auto_Binge_Hours),sum (Watch_Hours)),4) as Prc_Auto_Binge,
+round(safe_divide(sum(Up_Next_Hours), sum (Watch_Hours)),4) as Prc_Up_Next
+from cte
+group by 1, 2) as cte1
+where Grand_Total_Watch_Hours is not null
+order by 5 desc
