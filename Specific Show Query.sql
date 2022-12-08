6d323c02f28fc890312578ec75441e28111c2c63
ShunchaoChenNBC <117398619+ShunchaoChenNBC@users.noreply.github.com>
2022-12-07 21:19:23 -0500

    Update Specific Show Query.sql

--- Specific Show Query.sql
+++ Specific Show Query.sql
@@ -1,60 +1,31 @@
-with Raw_Clicks as (SELECT
-post_evar56 as Adobe_Tracking_ID, 
-DATE(timestamp(post_cust_hit_time_gmt), "America/New_York") AS Adobe_Date,
-DATETIME(timestamp(post_cust_hit_time_gmt), "America/New_York") AS Adobe_Timestamp,
-post_evar19 as Player_Event,
-post_evar7 as Binge_Details
-FROM `nbcu-ds-prod-001.feed.adobe_clickstream` 
-WHERE post_evar56 is not null
-and post_cust_hit_time_gmt is not null 
-and post_evar7 is not null
-and post_evar7 not like "%display"
-and DATETIME(timestamp(post_cust_hit_time_gmt), "America/New_York") between '2022-11-24' and '2022-11-25')
+with cte as (select
+Adobe_Date,
+Adobe_Tracking_ID,
+adobe_timestamp,
+lower(b.Display_Name) as Vdo_Titles,
+case when Last_Actions = 'Auto-Play' and Video_Start_Type = 'Vdo_End' then num_seconds_played_no_ads/3600 end as Auto_Binge_Hours,
+case when Last_Actions = 'Clicked-Up-Next' and Video_Start_Type = 'Vdo_End' then num_seconds_played_no_ads/3600 end as Up_Next_Hours,
+num_seconds_played_no_ads/3600 as Watch_Hours
+from
+(SELECT a.*,
+lag(Video_Start_Type) over (partition by Adobe_Tracking_ID order by adobe_timestamp) as Last_Actions
+FROM `nbcu-ds-sandbox-a-001.Shunchao_Sandbox.Bumper in Berlin` a
+where Adobe_Tracking_ID is not null and Video_Start_Type is not null
+order by Adobe_Tracking_ID, adobe_timestamp) b
+where b.Display_Name is not null)
 
 select *
-from
-(SELECT 
-Adobe_Tracking_ID,
+from 
+(
+select 
 Adobe_Date,
-Adobe_Timestamp,
-Player_Event,
-Binge_Details,
-case when Binge_Details like "%auto-play" then "Auto-Play" 
-     when Binge_Details like '%cue%up%click' then "Clicked-Up-Next" 
-     when Binge_Details like "%dismiss" then "Dismiss" 
-     when Player_Event like "%details:%" and Binge_Details is not null then "Manual-Selection"
-     when Binge_Details like '%deeplink%' then "Manual-Selection_(DeepLink)"
-     when Binge_Details like 'rail%click'then "Manual-Selection_(RailClick)" 
-else null end as Video_Start_Type,
-'' device_name,
-'' Feeder_Video,
-'' Feeder_Video_Id,
-case when Player_Event like "%details:%" and Binge_Details is not null then REGEXP_REPLACE(Player_Event, r'\w+:', '')
-     when Binge_Details like "%auto-play" then  REGEXP_EXTRACT(Binge_Details, r"[|][|](.*)[|]")
-     when Binge_Details like '%cue%up%click' then  REGEXP_EXTRACT(Binge_Details, r"[|][|](.*)[|]")
-     when Binge_Details like 'rail%click'then REGEXP_EXTRACT(Binge_Details, r"[|]([a-zA-Z0-9\s-.:]+)[|]click")
-else null end as Display_Name,
-'' video_id,
-null num_seconds_played_no_ads
-FROM Raw_Clicks) cte1
-where lower(Display_Name) = 'pitch perfect: bumper in berlin'
-union all
-SELECT
-adobe_tracking_id as Adobe_Tracking_ID,
-adobe_date as Adobe_Date,
-TIMESTAMP_ADD(adobe_timestamp , INTERVAL -40 second) as Adobe_Timestamp,
-'' Player_Event,
-'' Binge_Details,
-'Vdo_End' as Video_Start_Type,
-device_name,
-Lag(display_name) over (partition by adobe_tracking_id,adobe_date order by adobe_timestamp) as Feeder_Video,
-Lag(video_id) over (partition by adobe_tracking_id,adobe_date order by adobe_timestamp) as Feeder_Video_Id,
-display_name as Display_Name,
-video_id,
-num_seconds_played_no_ads
-FROM 
-`nbcu-ds-prod-001.PeacockDataMartSilver.SILVER_VIDEO`
-where adobe_tracking_ID is not null and lower(display_name) = 'pitch perfect: bumper in berlin'
-and adobe_date between '2022-11-24' and '2022-11-25'
-and media_load = False and num_seconds_played_with_ads > 0
-order by Adobe_Tracking_ID, Adobe_Timestamp;
+Vdo_Titles,
+round(sum(Auto_Binge_Hours),4) as Grand_Auto_Binge_Hours,
+round(sum(Up_Next_Hours),4) as Grand_Up_Next_Hours,
+round(sum (Watch_Hours),4) as Grand_Total_Watch_Hours,
+round(SAFE_DIVIDE(sum(Auto_Binge_Hours),sum (Watch_Hours)),4) as Prc_Auto_Binge,
+round(safe_divide(sum(Up_Next_Hours), sum (Watch_Hours)),4) as Prc_Up_Next
+from cte
+group by 1, 2) as cte1
+where Grand_Total_Watch_Hours is not null
+order by 5 desc
